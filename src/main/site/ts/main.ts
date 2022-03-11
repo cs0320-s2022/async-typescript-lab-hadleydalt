71b01c7d727c1da7e3d7070537c01e0b4a835ef1
hadleydalt <77938899+hadleydalt@users.noreply.github.com>
2022-03-11 18:06:38 -0500

    lab 5

--- src/main/site/ts/main.ts
+++ src/main/site/ts/main.ts
@@ -1,21 +1,40 @@
 // TODO: select the list element where the suggestions should go, and all three dropdown elements
 //  HINT: look at the HTML
+const suggestionList = document.querySelector('#suggestions') as HTMLElement
+const dropDownSun = document.querySelector('#sun') as HTMLOptionElement
+const dropDownMoon = document.querySelector('#moon') as HTMLOptionElement
+const dropDownRising = document.querySelector('#rising') as HTMLOptionElement
 
 // Here, when the value of sun is changed, we will call the method postAndUpdate.
 // TODO: Do the same for moon and rising
 
+dropDownSun.addEventListener('change', postAndUpdate);
+dropDownMoon.addEventListener('change', postAndUpdate);
+dropDownRising.addEventListener('change', postAndUpdate);
+
 // TODO: Define a type for the request data object here.
-// type MatchesRequestData = {}
+type MatchesRequestData = {
+  sun: string
+  moon: string
+  rising: string
+}
 
 // TODO: Define a type for the response data object here.
-// type Matches = {}
+type Matches = {
+  matches: string[]
+}
 
 function postAndUpdate(): void {
   // TODO: empty the suggestionList (you want new suggestions each time someone types something new)
   //  HINT: use .innerHTML
+  suggestionList.innerHTML = ""
 
   // TODO: add a type annotation to make this of type MatchesRequestData
-  const postParameters = {
+
+  const postParameters: MatchesRequestData = {
+    sun: dropDownSun.value,
+    moon: dropDownMoon.value,
+    rising: dropDownRising.value
     // TODO: get the text inside the input box
     //  HINT: use sun.value to get the value of the sun field, for example
   };
@@ -30,6 +49,22 @@ function postAndUpdate(): void {
   // TODO: Call and fill in the updateSuggestions method in one of the .then statements in the Promise
   //  Parse the JSON in the response object
   //  HINT: remember to get the specific field in the JSON you want to use
+
+  fetch('http://localhost:4567/results', {
+    method: 'post',
+    body: JSON.stringify({
+      sun: postParameters.sun,
+      moon: postParameters.moon,
+      rising: postParameters.rising
+    }),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      "Access-Control-Allow-Origin": "*"
+    }
+  })
+      .then((response) => response.json())
+      .then((data: Matches) => {updateSuggestions(data.matches)
+      console.log(data.matches)});
 }
 
 function updateSuggestions(matches: string[]): void {
@@ -38,6 +73,7 @@ function updateSuggestions(matches: string[]): void {
   //  NOTE: you should use <li> (list item) tags to wrap each element. When you do so,
   //  make sure to add the attribute 'tabindex="0"' (for example: <li tabindex="0">{your element}</li>).
   //  This makes each element selectable via screen reader.
+  matches.map((match) => suggestionList.innerHTML += `<li tabindex="0">${match}</li>`)
 }
 
 // TODO: create an event listener to the document (document.addEventListener) that detects "keyup".
@@ -45,13 +81,17 @@ function updateSuggestions(matches: string[]): void {
 //  values for the sun, moon, and rising using updateValues. Then call postAndUpdate().
 //  HINT: the listener callback function should be asynchronous and wait until the values are
 //  updated before calling postAndUpdate().
+document.addEventListener("keyup", async()=>{
+  await updateValues("Scorpio", "Gemini", "Scorpio")
+  postAndUpdate()
+})
 
 async function updateValues(sunval: string, moonval: string, risingval: string): Promise<void>{
   // This line asynchronously waits 1 second before updating the values.
   // It's unnecessary here, but it simulates asynchronous behavior you often have to account for.
   await new Promise(resolve => setTimeout(resolve, 1000));
 
-  sun.value = sunval;
-  moon.value = moonval;
-  rising.value = risingval;
+  dropDownSun.value = sunval;
+  dropDownMoon.value = moonval;
+  dropDownRising.value = risingval;
 }
